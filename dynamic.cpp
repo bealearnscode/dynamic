588c3a2b6d5a0937db40bc74d46e803421108802
Beatrix <bealearnscode@gmail.com>
2019-03-19 19:11:40 +0000

    dynamically allocating array values

--- dynamic.cpp
+++ dynamic.cpp
@@ -24,40 +24,49 @@ int main()
    two = new int;
    
    //  Fill in code to allocate the integer variable three here
-   
   three = new int;
+  
    //  Fill in code to allocate the character array pointed by name
+   name = new char[MAXNAME];
 
    cout << "Enter your last name with exactly 10 characters." << endl;
    cout << "If your name has < 10 characters, repeat last letter. " << endl
 		<< "Blanks at the end do not count." << endl;
    
    for ( pos = 0;  pos < MAXNAME;   pos++)
-
-	  cin >> // Fill in code to read a character into the name array
-                // WITHOUT USING a bracketed subscript
+   
+   // Fill in code to read a character into the name array
+   // WITHOUT USING a bracketed subscript
+	  //cin >> *(name + pos);
+	  cin >> name[pos];
 
    cout << "Hi ";
    for ( pos = 0;  pos < MAXNAME;  pos++)
 
-      cout << // Fill in code to a print a character from the name array
-              // WITHOUT USING a bracketed subscript
+   // Fill in code to a print a character from the name array
+   // WITHOUT USING a bracketed subscript
+      //cout << *(name + pos);
+      cin >> name[pos];
   
    cout << endl << "Enter three integer numbers separated by blanks" << endl;
 
    // Fill in code to input three numbers and store them in the
    // dynamic variables pointed to by pointers one, two, and three.
    // You are working only with pointer variables
+   cin >> *one >> *two >> *three;
 
    //echo print
-   cout << "The three numbers are " << endl;
-
    // Fill in code to output those numbers
+   cout << "The three numbers are " << *one << " " << *two << " " << *three << endl;
 
-   result = // Fill in code to calculate the sum of the three numbers
+   result = *one + *two + *three; // Fill in code to calculate the sum of the three numbers
    cout << "The sum of the three values is " << result << endl;
 
   // Fill in code to deallocate one, two, three and name
+  delete one;
+  delete two;
+  delete three;
+  delete [] name;
 
 
 
